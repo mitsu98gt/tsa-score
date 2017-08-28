c8682d5a46725501ff11c84e7a13f7150b0adceb
Dang <opr1053@infarmbureau.com>
2017-08-28 13:56:36 -0400

    saving work

--- src/main/java/com/envisageconsulting/primefaces/scoredaddy/GSSFIndoorScoreSheetBean.java
+++ src/main/java/com/envisageconsulting/primefaces/scoredaddy/GSSFIndoorScoreSheetBean.java
@@ -1,10 +1,13 @@
 package com.envisageconsulting.primefaces.scoredaddy;
 
+import com.envisageconsulting.primefaces.scoredaddy.domain.Competitor;
 import com.envisageconsulting.primefaces.scoredaddy.domain.GSSFIndoorScoreSheet;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import java.util.List;
 
 @SessionScoped
 @ManagedBean (name = "scoresheetBean")
@@ -12,9 +15,35 @@ public class GSSFIndoorScoreSheetBean {
 
 	private GSSFIndoorScoreSheet scoreSheet;
 
+    @ManagedProperty("#{competitorDataSource}")
+    private CompetitorDataSource ds;
+
+    private Competitor competitor;
+
     @PostConstruct
     public void init() {
 
+    }
+
+    public List<Competitor> complete(String query){
+        // Assumed Datasource
+        return ds.queryByName(query);
+    }
+
+    public Competitor getCompetitor() {
+        return competitor;
+    }
+
+    public void setCompetitor(Competitor competitor) {
+        this.competitor = competitor;
+    }
+
+    public CompetitorDataSource getDs() {
+        return ds;
+    }
+
+    public void setDs(CompetitorDataSource ds) {
+        this.ds = ds;
     }
 
     public GSSFIndoorScoreSheet getScoreSheet() {
