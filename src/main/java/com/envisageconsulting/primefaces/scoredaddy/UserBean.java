a784bb78ec1c3caced092fdef3f5bfcd81fe8ead
Dang <opr1053@infarmbureau.com>
2017-09-18 15:55:24 -0400

    adding serializable

--- src/main/java/com/envisageconsulting/primefaces/scoredaddy/UserBean.java
+++ src/main/java/com/envisageconsulting/primefaces/scoredaddy/UserBean.java
@@ -13,11 +13,12 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
 @ManagedBean(name="userBean")
 @SessionScoped
-public class UserBean {
+public class UserBean implements Serializable {
 
     private User user;
     private List<UserRole> roles;
