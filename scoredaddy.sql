337783cff668c9cc062565b4269b533df1a50ee1
Vinh Dang <vinh@envisageconsulting.com>
2018-01-04 17:43:28 -0500

    saving export

--- scoredaddy.sql
+++ scoredaddy.sql
@@ -239,7 +239,7 @@ CREATE TABLE `competitor` (
   `gssf_id` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -452,4 +452,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-04  2:20:12
+-- Dump completed on 2018-01-04 17:42:45
