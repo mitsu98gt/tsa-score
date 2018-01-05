a49ffcd145c0aa5a1c49ade7a42f4ddec9cf018a
Vinh Dang <vinh@envisageconsulting.com>
2018-01-04 22:43:40 -0500

    saving work

--- src/main/java/com/envisageconsulting/primefaces/scoredaddy/dao/impl/CompetitionResultsDAOImpl.java
+++ src/main/java/com/envisageconsulting/primefaces/scoredaddy/dao/impl/CompetitionResultsDAOImpl.java
@@ -107,7 +107,7 @@ public class CompetitionResultsDAOImpl implements CompetitionResultsDAO {
         }
     }
 
-    public List<CompetitionResults> getAverageTwoCompetitionResultsByCopetitorIdAndDivision(int competitorId, String divisionName, int division, int competition1d1, int competitionId2) throws Exception {
+    public List<CompetitionResults> getAverageTwoCompetitionResultsByCopetitorIdAndDivision(int competitorId, String divisionName, int division, int competitionId1, int competitionId2) throws Exception {
 
         List<CompetitionResults> competitionResultsList = new ArrayList<CompetitionResults>();
 
@@ -122,58 +122,58 @@ public class CompetitionResultsDAOImpl implements CompetitionResultsDAO {
         try {
             conn = dataSource.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
-            
-            ps.setInt(1, 1);
-            ps.setInt(2, 1);
-            ps.setInt(3, 1);
-            ps.setInt(4, 1);
-            ps.setInt(5, 1);
-            ps.setInt(6, 1);
-            ps.setInt(7, 1);
-            ps.setInt(8, 1);
-            ps.setInt(9, 1);
-            ps.setInt(10, 1);
-            ps.setInt(11, 1);
-            ps.setInt(12, 1);
-            ps.setInt(13, 1);
-            ps.setInt(14, 1);
-            ps.setInt(15, 1);
-            ps.setInt(16, 1);
-            ps.setInt(17, 1);
-            ps.setInt(18, 1);
-            ps.setInt(19, 1);
-            ps.setInt(20, 1);
-            ps.setInt(21, 1);
-            ps.setInt(22, 1);
-            ps.setInt(23, 1);
-            ps.setInt(24, 1);
-            ps.setInt(25, 1);
-            ps.setInt(26, 1);
-            ps.setInt(27, 1);
-            ps.setInt(28, 1);
-            ps.setInt(29, 1);
-            ps.setInt(30, 1);
-            ps.setInt(31, 1);
-            ps.setInt(32, 1);
-            ps.setInt(33, 1);
-            ps.setInt(34, 1);
-            ps.setInt(35, 1);
-            ps.setInt(36, 1);
-            ps.setInt(37, 1);
-            ps.setInt(38, 1);
-            ps.setInt(39, 1);
-            ps.setInt(40, 1);
-            ps.setInt(41, 1);
-            ps.setInt(42, 1);
-            ps.setInt(43, 1);
-            ps.setInt(44, 1);
-            ps.setInt(45, 1);
-            ps.setInt(46, 1);
-            ps.setInt(47, 1);
-            ps.setInt(48, 1);
-            ps.setInt(49, 1);
-            ps.setInt(50, 1);
-            ps.setInt(51, 1);
+
+            ps.setInt(1, competitorId);
+            ps.setInt(2, competitionId1);
+            ps.setInt(3, competitionId2);
+            ps.setInt(4, competitorId);
+            ps.setInt(5, competitorId);
+            ps.setInt(6, competitionId1);
+            ps.setInt(7, competitionId2);
+            ps.setInt(8, competitorId);
+            ps.setInt(9, competitorId);
+            ps.setInt(10, competitionId1);
+            ps.setInt(11, competitionId2);
+            ps.setInt(12, competitorId);
+            ps.setInt(13, competitorId);
+            ps.setInt(14, competitionId1);
+            ps.setInt(15, competitionId2);
+            ps.setInt(16, competitorId);
+            ps.setInt(17, competitorId);
+            ps.setInt(18, competitionId1);
+            ps.setInt(19, competitionId2);
+            ps.setInt(20, competitorId);
+            ps.setInt(21, competitionId1);
+            ps.setInt(22, competitionId2);
+            ps.setInt(23, competitorId);
+            ps.setInt(24, competitionId1);
+            ps.setInt(25, competitionId2);
+            ps.setInt(26, competitorId);
+            ps.setInt(27, competitionId1);
+            ps.setInt(28, competitionId2);
+            ps.setInt(29, competitorId);
+            ps.setInt(30, competitionId1);
+            ps.setInt(31, competitionId2);
+            ps.setInt(32, competitorId);
+            ps.setInt(33, competitionId1);
+            ps.setInt(34, competitionId2);
+            ps.setInt(35, competitorId);
+            ps.setInt(36, competitionId1);
+            ps.setInt(37, competitionId2);
+            ps.setInt(38, competitorId);
+            ps.setInt(39, competitorId);
+            ps.setInt(40, competitionId1);
+            ps.setInt(41, competitionId2);
+            ps.setInt(42, competitorId);
+            ps.setInt(43, competitorId);
+            ps.setInt(44, competitionId1);
+            ps.setInt(45, competitionId2);
+            ps.setInt(46, competitorId);
+            ps.setInt(47, competitionId1);
+            ps.setInt(48, competitionId2);
+            ps.setInt(49, competitorId);
+            ps.setInt(50, competitionId1);
+            ps.setInt(51, competitionId2);
 
             ResultSet rs = ps.executeQuery();
             int rank = 0;
